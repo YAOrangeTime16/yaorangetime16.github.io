5e058fcb090ba9ea701e83877017b897bed3479a
YAOrangeTime16 <yokoandrae@gmail.com>
2017-04-24 20:48:27 +0200

    modified html and changed main photo

--- js/main.js
+++ js/main.js
@@ -1,47 +1,10 @@
 $(function(){
-    let num=0;
-    let numCV=0;
-    let numP=0;
-     $('#about').on('click', function(){
-
-        $(this).data('click', ++num);
-         click=$(this).data('click');
-            if(click===1){
-                $('#popinAbout').load('about.html');
-                $('#cv, #work, .h1_who').fadeToggle('fast');
-            }else{
-                $('#popinAbout').fadeToggle();
-                $('#cv, #work, .h1_who').fadeToggle('fast');
-            }
-        
-    });
-
-    $('#cv').on('click', function(){
-        $(this).data('click', ++numCV);
-         click=$(this).data('click');
-            if(click===1){
-                $('#popinCV').load('background.html');
-                $('#work, .h1_cv').fadeToggle('fast');
-            }else{
-                $('#popinCV').fadeToggle();
-                $('#work, .h1_cv').fadeToggle('fast');
-            }
-        
-    });
-    
-    $('#work').on('click', function(){
-        $(this).data('click', ++numP);
-         click=$(this).data('click');
-            if(click===1){
-                $('#popinWork').load('projects.html');
-                $('.h1_work').fadeToggle('fast');
-            }else{
-                $('#popinWork').fadeToggle();
-                $('.h1_work').fadeToggle('fast');
-            }
-    });
-    
-    $('#back-to-top').on('click', function(){
-        $('html, body').animate({scrollTop: 0});
-    })
+  $('a[href^=#]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
 });
